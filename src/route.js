a32a696f787fac61e72f652ba6da926553fd464a
BeeMOre32 <windowssart01@gmail.com>
2023-02-10 13:53:11 +0900

    feat: making master sold item list page

--- src/route.js
+++ src/route.js
@@ -1,11 +1,10 @@
 import Navigo from "navigo";
 import { renderMasterPage } from "./page/master";
 import { renderAddProduct } from "./page/massteraddproduct";
-import { renderMainPage } from "./page/main"
-import { renderDetailPage } from "./page/productDetail"; 
+import { renderMainPage } from "./page/main";
+import { renderDetailPage } from "./page/productDetail";
 import { renderMasterProductDetailPage } from "./page/masterproductdetail";
-import { renderOrderHisory } from "./page/mypage";
-
+import { renderSoldProduct } from "./page/mastersoldlist";
 
 export const router = new Navigo("/");
 const app = document.querySelector("#app");
@@ -30,8 +29,8 @@ router.on("/product/checkout", function () {
   console.log("checkout");
 });
 
-router.on("/mypage/orderhistory", function (params) {
-  renderOrderHisory(params);
+router.on("/mypage", function () {
+  app.innerHTML = `<h1>My Page</h1>`;
 });
 
 router.on("/mypage/bank", function () {
@@ -52,6 +51,10 @@ router.on("/master/product/detail/:id", function (match) {
 
 router.on("/master/product/add", function () {
   renderAddProduct();
+});
+
+router.on("/master/sold", function () {
+  renderSoldProduct();
 });
 
 router.resolve();
