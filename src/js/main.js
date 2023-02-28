208b773dda4a7a0419d1b8229ee306a4f36c5641
BeeMOre32 <windowssart01@gmail.com>
2023-02-28 17:16:38 +0900

    fix: fix bug unexpected result

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,58 @@
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/swiper.min.css';
-import 'swiper/modules/navigation/navigation-element.min.css'
-import 'swiper/modules/pagination/pagination-element.min.css'
+import Swiper from "swiper";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/modules/navigation/navigation-element.min.css";
+import "swiper/modules/pagination/pagination-element.min.css";
 
 import { getMasterProductList } from "../utilities/masterapi";
 
-const visualSliderEl = document.querySelector('.visual .swiper')
+const visualSliderEl = document.querySelector(".visual .swiper");
 
-const newItemSliderEl = document.querySelector('.new-items .swiper')
-const newItemSliderWrapperEl = newItemSliderEl.querySelector('.swiper-wrapper')
+const newItemSliderEl = document.querySelector(".new-items .swiper");
+const newItemSliderWrapperEl = newItemSliderEl.querySelector(".swiper-wrapper");
 
-;(
-  async function () {
-    const res = await getMasterProductList();
-    products = res;
-    console.log(res);
+(async function () {
+  const res = await getMasterProductList();
 
-    renderNewItems(res)
-  }
-)();
+  renderNewItems(res);
+})();
 
 function renderNewItems(items) {
-  items = items.filter((item) =>  item.price > 10000)
+  items = items.filter((item) => item.price > 10000);
 
   items.forEach((item) => {
-    const divEl = document.createElement('div');
-    const thumbnailEl = document.createElement('div');
-    const itemNameEl = document.createElement('h3');
+    const divEl = document.createElement("div");
+    const thumbnailEl = document.createElement("div");
+    const itemNameEl = document.createElement("h3");
 
     itemNameEl.textContent = item.title;
 
     thumbnailEl.style.backgroundImage = `url(${item.thumbnail})`;
 
-    divEl.classList.add('swiper-slide');
-    thumbnailEl.classList.add('thumbnail');
+    divEl.classList.add("swiper-slide");
+    thumbnailEl.classList.add("thumbnail");
 
-    divEl.append(thumbnailEl, itemNameEl)
+    divEl.append(thumbnailEl, itemNameEl);
     newItemSliderWrapperEl.append(divEl);
-  })
+  });
 }
-    
+
 new Swiper(newItemSliderEl, {
   // loop : true,
-  slidesPerView : 4,
+  slidesPerView: 4,
   spaceBetween: 15,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  autoplay: true
+  autoplay: true,
 });
 
 new Swiper(visualSliderEl, {
   // loop : true,
   slidesPerView: 1,
-  speed : 1000,
-  autoplay:  {
+  speed: 1000,
+  autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
@@ -65,8 +61,8 @@ new Swiper(visualSliderEl, {
     prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el : '.swiper-pagination',
-  }
-})
+    el: ".swiper-pagination",
+  },
+});
 
-console.log(visualSliderEl)
+console.log(visualSliderEl);
